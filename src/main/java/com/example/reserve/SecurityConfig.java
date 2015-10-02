ee0ae2ed6f70cac7933ae3752e7a5dcbf0d7a465
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-10-02 01:45:16 -0500

    refine datepicker in lodge and experience detail page

--- src/main/java/com/example/reserve/SecurityConfig.java
+++ src/main/java/com/example/reserve/SecurityConfig.java
@@ -27,7 +27,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 		"/experience/search", "/experience/detail", "/experience/addtocart", 
                 		"/shoppingcart/**", "/image/view", "/duima/**", "/register", 
                 		"/inquiry", "/howto", "/question", "/shoppingcart	",
-                		"/img/*", "/js/*", "/fonts/*", "/css/*", "/test").permitAll()
+                		"/img/**", "/js/**", "/fonts/**", "/css/**", "/test").permitAll()
                 .antMatchers("/users/**").hasAuthority("ADMIN")
                 .anyRequest().fullyAuthenticated()
                 .and()
